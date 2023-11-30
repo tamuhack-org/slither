6f02f8a565a55ed452a70366c7c3f49f86429619
NitroGuy10 <naviyer10@gmail.com>
2023-11-30 16:22:15 -0600

    Funny animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { left: '-20px' },
+          '50%': { left: '20px' },
+        }
+      },
+      animation: {
+        wiggle0: 'wiggle 1s ease-in-out 0.05s infinite',
+        wiggle1: 'wiggle 1s ease-in-out 0.1s infinite',
+        wiggle2: 'wiggle 1s ease-in-out 0.15s infinite',
+        wiggle3: 'wiggle 1s ease-in-out 0.2s infinite',
+        wiggle4: 'wiggle 1s ease-in-out 0.25s infinite',
+        wiggle5: 'wiggle 1s ease-in-out 0.3s infinite',
+        wiggle6: 'wiggle 1s ease-in-out 0.35s infinite',
+        wiggle7: 'wiggle 1s ease-in-out 0.4s infinite',
+        wiggle8: 'wiggle 1s ease-in-out 0.45s infinite',
+        wiggle9: 'wiggle 1s ease-in-out 0.5s infinite',
+      }
+    },
   },
   plugins: [],
 }
