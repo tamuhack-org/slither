5a77275979a96e75a56f485b1a6ec19c9190a742
NitroGuy10 <naviyer10@gmail.com>
2023-12-23 17:37:45 -0600

    Obos QR code type checking

---
+++ src/lib/logo.svelte
@@ -0,0 +1,13 @@
+
+<div class="relative h-96 select-none">
+    <h1 class="absolute left-[-20px] top-0 text-[250px] font-black text-pink-200 animate-wiggle0">Slither</h1>
+    <h1 class="absolute left-[-20px] top-1 text-[250px] font-black text-pink-300 animate-wiggle1">Slither</h1>
+    <h1 class="absolute left-[-20px] top-2 text-[250px] font-black text-pink-400 animate-wiggle2">Slither</h1>
+    <h1 class="absolute left-[-20px] top-3 text-[250px] font-black text-pink-500 animate-wiggle3">Slither</h1>
+    <h1 class="absolute left-[-20px] top-4 text-[250px] font-black text-pink-600 animate-wiggle4">Slither</h1>
+    <h1 class="absolute left-[-20px] top-5 text-[250px] font-black text-blue-600 animate-wiggle5">Slither</h1>
+    <h1 class="absolute left-[-20px] top-6 text-[250px] font-black text-blue-500 animate-wiggle6">Slither</h1>
+    <h1 class="absolute left-[-20px] top-7 text-[250px] font-black text-blue-400 animate-wiggle7">Slither</h1>
+    <h1 class="absolute left-[-20px] top-8 text-[250px] font-black text-blue-300 animate-wiggle8">Slither</h1>
+    <h1 class="absolute left-[-20px] top-9 text-[250px] font-black text-blue-200 animate-wiggle9">Slither</h1>     
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,36 @@
     import { Html5QrcodeScanner, Html5QrcodeScanType, type Html5QrcodeResult } from "html5-qrcode";
     import type { Html5QrcodeScannerConfig } from "html5-qrcode/esm/html5-qrcode-scanner";
 
+    type ObosQRCode = {
+        first_name: string;
+        last_name: string;
+        email: string;
+        university: string;
+    };
+
+    function getObosQRCode(decodedText: string): ObosQRCode | null {
+        try {
+            const decoded = JSON.parse(decodedText);
+            if (decoded.first_name && decoded.last_name && decoded.email && decoded.university) {
+                return decoded;
+            }
+        } catch (e) {
+            return null;
+        }
+        return null;
+    }
+
     function onScanSuccess(decodedText: string, decodedResult: Html5QrcodeResult) {
         console.log(decodedText);
+        const obosQRCode = getObosQRCode(decodedText);
+        if (obosQRCode)
+        {
+            alert(`Welcome ${obosQRCode.first_name} ${obosQRCode.last_name} from ${obosQRCode.university}!`);
+        }
+        else
+        {
+            alert("Invalid QR Code");
+        }
     }
 
     function onScanError(errorMessage: string) {
@@ -28,22 +56,12 @@
 </script>
 
 <div>
-    <div class="relative h-96">
-        <h1 class="absolute left-[-20px] top-0 text-[250px] font-black text-pink-200 animate-wiggle0">Slither</h1>
-        <h1 class="absolute left-[-20px] top-1 text-[250px] font-black text-pink-300 animate-wiggle1">Slither</h1>
-        <h1 class="absolute left-[-20px] top-2 text-[250px] font-black text-pink-400 animate-wiggle2">Slither</h1>
-        <h1 class="absolute left-[-20px] top-3 text-[250px] font-black text-pink-500 animate-wiggle3">Slither</h1>
-        <h1 class="absolute left-[-20px] top-4 text-[250px] font-black text-pink-600 animate-wiggle4">Slither</h1>
-        <h1 class="absolute left-[-20px] top-5 text-[250px] font-black text-blue-600 animate-wiggle5">Slither</h1>
-        <h1 class="absolute left-[-20px] top-6 text-[250px] font-black text-blue-500 animate-wiggle6">Slither</h1>
-        <h1 class="absolute left-[-20px] top-7 text-[250px] font-black text-blue-400 animate-wiggle7">Slither</h1>
-        <h1 class="absolute left-[-20px] top-8 text-[250px] font-black text-blue-300 animate-wiggle8">Slither</h1>
-        <h1 class="absolute left-[-20px] top-9 text-[250px] font-black text-blue-200 animate-wiggle9">Slither</h1>     
-    </div>
+    <h1 class="text-8xl text-center font-bold">Slither</h1>
+    <h2 class="text-3xl text-center font-semibold mb-28">QR Code Scanner</h2>
     
     {#if !scannerOpen}
-        <button class="text-9xl border-8 border-black rounded-lg p-2" on:click={() => {scannerOpen = true; makeScanner();}}>Start</button>
+        <button class="text-4xl border-4 border-black rounded-xl p-2 mx-auto block" on:click={() => {scannerOpen = true; makeScanner();}}>Start Scanning</button>
     {/if}
     
-    <div id="reader" class="w-full max-w-lg"></div>
+    <div id="reader" class="w-full max-w-2xl mx-auto"></div>
 </div>
